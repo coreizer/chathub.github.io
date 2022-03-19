761c4af23d26b30755085bdd56b3b41efd2b66bf
coreizer <coreizer@outlook.com>
2022-03-20 03:07:36 +0900

    ダークからライトに変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'body': ['"M PLUS 1"', 'sans-serif']
+    },
     extend: {
       colors: {
         bf2042: "#00F6FF"
